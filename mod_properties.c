8ab2a68aa2d2d1692761beb2100df1f485a3ef4f
nyarmith <powah.serge@gmail.com>
2017-11-12 22:03:25 -0500

    painful

---
+++ mod_properties.c
@@ -0,0 +1,84 @@
+#include <curses.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DELAY 180000
+
+void display_distributive_property(){
+}
+
+void display_additive_property(){
+}
+
+void print_mod(int dividend, int divisor){
+        attron(A_BOLD);
+        wprintw(stdscr, "%2d", dividend);
+        attroff(A_BOLD);
+
+        attron(A_ITALIC | A_BOLD);
+        wprintw(stdscr, " mod ");
+        attroff(A_ITALIC | A_BOLD);
+
+        attron(A_BOLD);
+        wprintw(stdscr, "%2d ",divisor);
+        attroff(A_BOLD);
+
+        attron(A_BLINK | A_DIM);
+        wprintw(stdscr, " = ");
+        attroff(A_BLINK | A_DIM);
+
+        attron(A_BOLD);
+        wprintw(stdscr, "%2d", dividend % divisor);
+        attroff(A_BOLD);
+}
+
+int main(int argc, char** argv){
+    if (argc < 4){
+        printf ("usage: %s <start_dividend> <stop_dividend> <start_divisor> <stop_divisor> [multiple]", argv[0]);
+        return 0;
+    }
+    int start_dividend = atoi(argv[1]);
+    int stop_dividend  = atoi(argv[2]);
+    int start_divisor  = atoi(argv[3]);
+    int stop_divisor   = atoi(argv[4]);
+    int multiple       = (argc==6) ? atoi(argv[5]) : 1;
+
+    initscr();
+
+    int line = 1;
+    while(start_dividend <= stop_dividend){
+        move(line, 2);
+
+        int fst = 0;
+        //2nd dim
+        int divisor_temp = start_divisor;
+        while(divisor_temp <= stop_divisor){
+            if (fst != 0){
+                wprintw(stdscr, "  ");
+                attron(A_ALTCHARSET | A_BOLD);
+                wprintw(stdscr, "a");
+                attroff(A_ALTCHARSET | A_BOLD);
+                wprintw(stdscr, " ");
+            } else {
+                fst = 1;
+            }
+            print_mod(start_dividend, divisor_temp);
+            refresh();
+            divisor_temp += multiple;
+            usleep( DELAY );
+
+        }
+
+        line++;
+        start_dividend += multiple;
+
+        usleep(DELAY);
+    }
+
+    mvprintw( LINES - 2, 3, "press any key to exit..");
+    getch();
+    endwin();
+
+    return 0;
+}
---
+++ mouse_test.cpp
@@ -0,0 +1,85 @@
+#include <ncurses.h>
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <tuple>
+
+const unsigned int DELAY = 1000;
+
+
+enum game_state { RUN, QUIT };
+game_state GAME = RUN;
+
+int get_sign (int a){ if (a == 0) return 1; return (a / abs(a)); }
+
+struct Ball{
+  Ball() : y(0), x(0) { };
+  static const int BALL_SCALE = 5;
+  int y, x;
+  void moveTo(int oy,int ox){
+    int ydiff = oy - y;
+    int xdiff = ox - x;
+    y = y + get_sign(ydiff) * ceil (double(abs(ydiff)) / (BALL_SCALE * BALL_SCALE));
+    x = x + get_sign(xdiff) * ceil (double(abs(xdiff)) / (BALL_SCALE * BALL_SCALE));
+//    if (oy < y)
+//      y--;
+//    else if (oy > y)
+//      y++;
+//    if (ox < x)
+//      x--;
+//    else if (ox > x)
+//      x++;
+  }
+};
+
+
+
+std::tuple<int,int> get_mousedown_xy(){
+  MEVENT m;
+  int c = getch();
+  if (c == KEY_MOUSE && getmouse(&m) == OK){
+    return std::make_tuple(m.y, m.x);
+  }
+  return std::make_tuple(-1,-1);
+}
+
+/*
+std::tuple<int,int> get_xy_movement(){
+  MEVENT* m = NULL;
+
+  return std::make_tuple(3,3);
+}
+*/
+
+
+
+int main(){
+  initscr();
+  cbreak();
+  keypad(stdscr, TRUE);
+  nodelay(stdscr, TRUE);
+  mousemask(ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION, NULL); //REPORT_MOUSE_POSITION is not in ALL_MOUSE_EVENTS!!!
+  //printf("\033[?1003h\n");
+  //printf("\e[?1000l\e[?1005l\e[?1015l");
+  printf("\x1b[<0;76;32m");
+  noecho(); //if there's an echo, you'll eventually print out characters that close the terminal
+
+  Ball the_ball;
+  the_ball.x =  5 + rand()%10;
+  the_ball.y =  5 + rand()%10;
+
+  std::tuple<int, int> mouse;
+
+  while( GAME != QUIT ) {
+    mouse = get_mousedown_xy();
+    if (std::get<0>(mouse) != -1){
+      the_ball.moveTo(std::get<0>(mouse),std::get<1>(mouse));
+    }
+
+    clear();
+    mvaddch(the_ball.y, the_ball.x, 'o');
+    refresh();
+    usleep( DELAY ); //if I don't add a delay, it crashes. I wonder why?(try gdb thing with separate stdout and stdin)
+  }
+}
